b40badfcb4f6295b41f4047d7ff66821eb5b856a
cex <cex@cexs-MBP.lan>
2021-03-17 22:18:39 +0000

    Status Update - 22:18

--- python-api.py
+++ python-api.py
@@ -16,6 +16,20 @@ from flask_restful import Resource, Api, reqparse
 apl = Flask(__name__)
 api = Api(apl)
 
+class Numbers(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('num', required=True)  # add args
+        args = parser.parse_args()  # parse arguments to dictionary
+
+	if args['num'] % 7 == 0:
+            return {
+                'message': f"E"
+            }, 200
+	elif: 
+
+	#return {'message': Welcome, enter your number."}, 200  # return msg & 200 OK
+
 
 api.add_resource(Numbers, '/numbers')  # add endpoints
 
