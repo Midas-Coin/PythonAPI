5758265b3888bca26f4be7c3cfb3b779c2cdd3c7
cex <cex@cexs-MBP.lan>
2021-03-17 21:35:46 +0000

    First Commit push - comment header file

---
+++ python-api.py
@@ -0,0 +1,13 @@
+#################
+# Python Get API
+# As per request
+# for the client
+#########################
+# Author: Fletcher Bayley
+# Note: Created for Dan
+# Sorry for being late!
+# Desc: Quick Python API
+# Date: 17/03/2021
+#########################
+
+
